da264b200106ece55a47705dae4170f4692eadf7
bachatron <idz420@gmail.com>
2022-05-03 18:55:44 -0300

    Main code finished. Needs some fixes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function computerPlay () {
     let randomNumber = Math.floor(Math.random()*10)
-    console.log (randomNumber)
+    //console.log (randomNumber)
     switch (true) {
         case randomNumber < 4:
             return "rock";
@@ -15,38 +15,74 @@ function playRound (playerSelection, computerSelection) {
     switch (true) {
         case playerSelection.toLowerCase() === "rock":
             if (computerSelection === "rock") {
-                return "It is a tie!";
+                alert("It is a tie!")
+                break
             }
             else if (computerSelection === "paper") {
-                return "Paper! You lose.";
+                pc += 1
+                alert("Paper! You lose.")
+                break
             }
             else if (computerSelection === "scissors") {
-                return "Scissors! You won!";
+                player += 1
+                alert ("Scissors! You won!")
+                break
             }
-            else return "That is not a valid option";
+            else {
+                alert ("That is not a valid option");
+                break}
         
         case playerSelection.toLowerCase() === "paper":
             if (computerSelection === "paper") {
-                return "It is a tie!";
+                alert ("It is a tie!")
+                break
             }
             else if (computerSelection === "scissors") {
-                return "Scissors! You lose.";
+                pc += 1
+                alert ("Scissors! You lose.")
+                break
             }
             else if (computerSelection === "rock") {
-                return "Rock! You won!";
+                player += 1
+                alert ("Rock! You won!")
+                break
             }
-            else return "That is not a valid option";
+            else {
+                alert ("That is not a valid option")
+                break}
         
         case playerSelection.toLowerCase() === "scissors":
             if (computerSelection === "scissors") {
-                return "It is a tie!";
+                alert ("It is a tie!")
+                break
             }
             else if (computerSelection === "rock") {
-                return "Rock! You lose.";
+                pc += 1
+                alert ("Rock! You lose.")
             }
             else if (computerSelection === "paper") {
-                return "Paper! You won!";
+                player += 1
+                alert ("Paper! You won!")
+                break
             }
-            else return "That is not a valid option";
+            else {
+                alert ("That is not a valid option")
+                break}
     }
 }
+
+function game(){
+    for (let i = 1; i < 6; i++) {
+        playRound(prompt('What do you chose?'), computerPlay())
+        alert (`Current results:\n Player: ${player} \n PC: ${pc} \n Game number ${i}`)
+        }
+    if (player > pc) {
+            alert ("You won!")} 
+        else if (player === pc) {alert ("Its a draw!")}
+        else alert("You lose")
+    }
+
+let player = 0
+let pc = 0
+
+game()
