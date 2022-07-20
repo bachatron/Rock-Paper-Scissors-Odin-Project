ff61ab655af7af079bb6462487f4450adfcdb369
bachatron <idz420@gmail.com>
2022-07-20 18:13:35 -0300

    HTML structure done. JS rock in progress.

--- js/script.js
+++ js/script.js
@@ -1,88 +1,46 @@
 function computerPlay () {
-    let randomNumber = Math.floor(Math.random()*10)
+    let randomNumber = 0
+    //Get a random number between 1 to 9
+    while (randomNumber === 0) {
+        randomNumber = Math.floor(Math.random()*10)
+    }
+    //Set the values for each range of number
     //console.log (randomNumber)
-    switch (true) {
-        case randomNumber < 4:
-            return "rock";
-        case randomNumber < 7:
-            return "paper";
-        default:
-            return "scissors";
+    if (randomNumber < 4) {
+        //console.log ("rock");
+        return 'rock'
+    } else if (randomNumber < 7) {
+        //console.log ("paper");
+        return 'paper'
+    } else if (randomNumber < 10){
+        //console.log ("scissors");
+        return 'scissors'
+    } 
+}    
+//This function check what happens when you choose play Rock.
+function playRock () {
+    let pcSelected = computerPlay();
+    if (pcSelected === 'rock') {
+        currentGame.innerHTML = "DRAW";
     }
+    else if (pcSelected === 'paper') {
+        pc += 1
+        currentGame.innerHTML = "Paper! You lose.";
+    }
+    else if (pcSelected === 'scissors') {
+        player += 1
+        currentGame.innerHTML = "Scissors! You win.";
+    }
+    score.innerHTML = 'PC: ' + pc + ' You: ' + player; 
 }
 
-function playRound (playerSelection, computerSelection) {
-    switch (true) {
-        case playerSelection.toLowerCase() === "rock":
-            if (computerSelection === "rock") {
-                alert("It is a tie!")
-                break
-            }
-            else if (computerSelection === "paper") {
-                pc += 1
-                alert("Paper! You lose.")
-                break
-            }
-            else if (computerSelection === "scissors") {
-                player += 1
-                alert ("Scissors! You won!")
-                break
-            }
-            else {
-                alert ("That is not a valid option");
-                break}
-        
-        case playerSelection.toLowerCase() === "paper":
-            if (computerSelection === "paper") {
-                alert ("It is a tie!")
-                break
-            }
-            else if (computerSelection === "scissors") {
-                pc += 1
-                alert ("Scissors! You lose.")
-                break
-            }
-            else if (computerSelection === "rock") {
-                player += 1
-                alert ("Rock! You won!")
-                break
-            }
-            else {
-                alert ("That is not a valid option")
-                break}
-        
-        case playerSelection.toLowerCase() === "scissors":
-            if (computerSelection === "scissors") {
-                alert ("It is a tie!")
-                break
-            }
-            else if (computerSelection === "rock") {
-                pc += 1
-                alert ("Rock! You lose.")
-            }
-            else if (computerSelection === "paper") {
-                player += 1
-                alert ("Paper! You won!")
-                break
-            }
-            else {
-                alert ("That is not a valid option")
-                break}
-    }
-}
 
-function game(){
-    for (let i = 1; i < 6; i++) {
-        playRound(prompt('What do you chose?'), computerPlay())
-        alert (`Current results:\n Player: ${player} \n PC: ${pc} \n Game number ${i}`)
-        }
-    if (player > pc) {
-            alert ("You won!")} 
-        else if (player === pc) {alert ("Its a draw!")}
-        else alert("You lose")
-    }
+let player = 0;
+let pc = 0;
 
-let player = 0
-let pc = 0
+const rockBtn = document.getElementById("rock");
+rockBtn.addEventListener('click', playRock);
 
-game()
+const score = document.getElementById('score');
+
+const currentGame = document.getElementById('currentGame');
